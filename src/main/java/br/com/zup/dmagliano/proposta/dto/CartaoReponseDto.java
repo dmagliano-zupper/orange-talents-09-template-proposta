d71670da7a635c0c03bb38b1048ed574c76f363c
Diogo Magliano <diogo.magliano@zup.com.br>
2021-10-31 11:03:41 -0300

    Reduzindo objetos serializados com IgnoreUnknow no JsonIgnoreProperties.

--- src/main/java/br/com/zup/dmagliano/proposta/dto/CartaoReponseDto.java
+++ src/main/java/br/com/zup/dmagliano/proposta/dto/CartaoReponseDto.java
@@ -1,20 +1,15 @@
 package br.com.zup.dmagliano.proposta.dto;
 
 import br.com.zup.dmagliano.proposta.model.Cartao;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CartaoReponseDto {
 
     private String id;
     private String emitidoEm;
     private String titular;
-    private List<BloqueiosDto> bloqueios;
-    private List<AvisosDto> avisos;
-    private List<CarteirasDto> carteiras;
-    private List<ParcelasDto> parcelas;
     private Integer limite;
-    private RenegociacaoDto renegociacao;
     private VencimentoDto vencimento;
     private String idProposta;
 
@@ -22,6 +17,15 @@ public class CartaoReponseDto {
     public CartaoReponseDto() {
     }
 
+    public CartaoReponseDto(String id, String emitidoEm, String titular, Integer limite,
+                            VencimentoDto vencimento, String idProposta) {
+        this.id = id;
+        this.emitidoEm = emitidoEm;
+        this.titular = titular;
+        this.limite = limite;
+        this.vencimento = vencimento;
+        this.idProposta = idProposta;
+    }
 
     public String getId() {
         return id;
@@ -33,10 +37,6 @@ public class CartaoReponseDto {
 
     public String getTitular() {
         return titular;
-    }
-
-    public List<AvisosDto> getAvisos() {
-        return avisos;
     }
 
     public Integer getLimite() {
