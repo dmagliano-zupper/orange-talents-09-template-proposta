28fcdb25130b013026a95553b25f56f5e3bfbf99
Diogo Magliano <diogo.magliano@zup.com.br>
2021-11-03 16:31:33 -0300

    Corrigindo testes para funcionar sem a autenticação JWT.

--- src/main/java/br/com/zup/dmagliano/proposta/security/SecurityConfiguration.java
+++ src/main/java/br/com/zup/dmagliano/proposta/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.zup.dmagliano.proposta.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 
 @Configuration
 @EnableWebSecurity
+@Profile(value = {"development", "production"})
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
--- src/test/java/br/com/zup/dmagliano/proposta/controller/PropostaControllerTest.java
+++ src/test/java/br/com/zup/dmagliano/proposta/controller/PropostaControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -20,9 +21,9 @@ import java.net.URI;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @Transactional
-class PropostaControllerTest {
+public class PropostaControllerTest {
 
     @Autowired
     private Gson gson;
