8651d3864d604303b21bd48f770601df74892d84
Diogo Magliano <diogo.magliano@zup.com.br>
2021-11-03 22:38:42 -0300

    Configurando a segurança para acesso ao prometheus.

--- src/main/java/br/com/zup/dmagliano/proposta/security/SecurityConfiguration.java
+++ src/main/java/br/com/zup/dmagliano/proposta/security/SecurityConfiguration.java
@@ -4,20 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-@EnableWebSecurity
-@Profile(value = {"development", "production"})
+@Profile(value = {"dev", "prod"})
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/actuator/**")
+                .permitAll()
+                .and()
+                .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                                 .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_login-scope")
                                 .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_login-scope")
                                 .antMatchers(HttpMethod.GET, "/cartoes/**").hasAuthority("SCOPE_login-scope")
@@ -25,8 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                 .antMatchers(HttpMethod.GET, "/biometria/**").hasAuthority("SCOPE_login-scope")
                                 .antMatchers(HttpMethod.POST, "/biometria/**").hasAuthority("SCOPE_login-scope")
                                 .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
 }
